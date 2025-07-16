9649b92a735bee1c8f23f218ff24f050a7bf865b
AHMED ALI MUGHAL <163463244+coderAAM@users.noreply.github.com>
2025-07-16 11:27:26 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+
+// Class No 1
+
+// check even or odd number in given condition statement
+// let num = prompt("Enter the Number ");// this number is odd so answer is odd
+// if (num % 2 == 0) {
+//      console.log("this number is even");
+// } else {
+//      console.log("this number is odd");
+// }
+//prompt this mean user output
+
+// let age = prompt("enter your age");
+// if (age >= 18) {
+//      console.log("You are eligble for vote")
+// } else {
+//      console.log("You are not elibble for vote")
+// }
+//this double line means is to show the comment
+
+// create a mine game for find the number ok!
+
+// let game = prompt("find the number:");
+// if (game == 4) { // yah number find karna hai
+//      console.log("Win");
+// } else {
+//      console.log("Loess");
+// }
+
+// ab hum student ke number store kare ge
+
+// let mark = prompt("enter your marks and find the grade");
+// if (mark >= 90 && mark <= 100) {
+//      console.log("Grade is A");
+// } else if (mark >= 80 && mark <= 89) {
+//      console.log("Grade B");
+// } else if (mark >= 70 && mark <= 79) {
+//      console.log("Grade C");
+// } else if (mark >= 60 && mark <= 69) {
+//      console.log("Grade D");
+// } else if (mark >= 0 && mark < 60) {
+//      console.log("Grade F");
+// } else {
+//      console.log("Invalid marks entered");
+// }
+
+// now the End class Buy buy my friend's take care 
